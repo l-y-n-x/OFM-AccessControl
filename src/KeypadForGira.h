4c3f7f508c3646109d44f5ff9220f67649ef7d8d
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-11-04 11:20:27 +0100

    Added LED control methods and some clean-up.

--- src/KeypadForGira.h
+++ src/KeypadForGira.h
@@ -20,6 +20,8 @@ class KeypadForGira
     void init(bool testMode = false);
     void loop(bool testMode = false);
     void registerCallback(KeyPressedCallback callback);
+    void setInfoLed(uint8_t red, uint8_t green, uint8_t blue);
+    void setBackgroundLed(uint8_t brightness);
 
 #ifdef KEYPAD_PCA9633_ADDR
     void runLedTestSequence();
@@ -27,6 +29,7 @@ class KeypadForGira
 
   private:
     char mapKey(uint8_t index) const;
+    void updateLeds();
 
     KeyPressedCallback _callback;
     bool _initialized = false;
@@ -36,5 +39,9 @@ class KeypadForGira
 #ifdef KEYPAD_PCA9633_ADDR
     PCA9633 _ledController = PCA9633(REG_PWM0, REG_PWM1, REG_PWM2, REG_PWM3);
     bool _ledInitialized = false;
+    uint8_t _ledRed = 0;
+    uint8_t _ledGreen = 0;
+    uint8_t _ledBlue = 0;
+    uint8_t _ledBackground = 0;
 #endif
 };
